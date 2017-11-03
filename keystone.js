f7d543df47e43215d9f9e8e69fd80d463afa0a9f
zoro <aoping@yougola.com>
2017-11-03 15:50:41 +0800

    init

---
+++ keystone.js
@@ -0,0 +1,38 @@
+// Simulate config options from your production environment by
+// customising the .env file in your project's root folder.
+require('dotenv').config();
+
+// Require keystone
+var keystone = require('keystone');
+
+keystone.init({
+	'name': 'keystone-admin',
+	'brand': 'keystone-admin',
+
+	'less': 'public',
+	'static': 'public',
+	'favicon': 'public/favicon.ico',
+	'views': 'templates/views',
+	'view engine': 'pug',
+
+	'auto update': true,
+	'session': true,
+	'auth': true,
+	'user model': 'N',
+});
+keystone.import('models');
+keystone.set('locals', {
+	_: require('lodash'),
+	env: keystone.get('env'),
+	utils: keystone.utils,
+	editable: keystone.content.editable,
+});
+keystone.set('routes', require('./routes'));
+
+keystone.set('nav', {
+	ns: 'ns',
+});
+
+
+
+keystone.start();
